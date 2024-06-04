15d04310af3d36e432c6571ce005e4a3398224fd
kimjaesung <aamoos@naver.com>
2024-06-04 15:35:52 +0900

    대문자 소문자 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO AUTHORITY(AUTHORITY_NAME) VALUES ('ROLE_ADMIN');
-INSERT INTO AUTHORITY(AUTHORITY_NAME) VALUES ('ROLE_USER');
-INSERT INTO USERS(ACTIVATED, PASSWORD, USER_ID, USER_NAME, USER_SEQ) VALUES(TRUE, '$2a$10$OJtaRJO5NDWAgAzZeGyfpuWGWxJ1BfnTwT0nmO9UNZ1O.IHPXfSfy', 'aamoos@naver.com', '김재성', '2');
-INSERT INTO USER_AUTHORITY(USER_SEQ, AUTHORITY_NAME) VALUES('2', 'ROLE_USER');
+insert into authority(authority_name) values ('ROLE_ADMIN');
+insert into authority(authority_name) values ('ROLE_USER');
+insert into users(activated, password, user_id, user_name, user_seq) values(true, '$2a$10$OJtaRJO5NDWAgAzZeGyfpuWGWxJ1BfnTwT0nmO9UNZ1O.IHPXfSfy', 'aamoos@naver.com', '김재성', '2');
+insert into user_authority(user_seq, authority_name) values('2', 'ROLE_USER');
