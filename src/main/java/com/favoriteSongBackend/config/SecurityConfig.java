6e9ec3de7b0113bcc4a975b575970c88b7625227
김재성 <aamoos@openmate.co.kr>
2024-06-10 15:51:35 +0900

    테스트

--- src/main/java/com/favoriteSongBackend/config/SecurityConfig.java
+++ src/main/java/com/favoriteSongBackend/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
 //                                .requestMatchers(PathRequest.toH2Console()).permitAll()           //h2 console 관련 security 허용
-                                .requestMatchers("/auth/**", "/oauth2/**", "/sendMail/**", "/login/oauth2/**/**").permitAll()
+                                .requestMatchers("/auth/**", "/sendMail/**").permitAll()
                                 .anyRequest().authenticated()
 //                                .anyRequest().permitAll()
                 )
