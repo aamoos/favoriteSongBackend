ea199657aec9152df63e1299ea81743f9736186c
kimjaesung <aamoos@naver.com>
2025-02-17 15:42:20 +0900

    cors

---
+++ src/main/java/com/favoriteSongBackend/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.favoriteSongBackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
+                .allowedOrigins("https://favorite-song-ten.vercel.app") // Vercel URL을 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드들
+                .allowedHeaders("*"); // 모든 헤더를 허용
+    }
+}
