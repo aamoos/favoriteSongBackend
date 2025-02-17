8cf3f8b11f3e16bd5fbc97cb86327a9ab0f9612d
kimjaesung <aamoos@naver.com>
2025-02-17 16:20:47 +0900

    test

--- src/main/java/com/favoriteSongBackend/FavoriteSongBackendApplication.java
+++ src/main/java/com/favoriteSongBackend/FavoriteSongBackendApplication.java
@@ -10,6 +10,20 @@ public class FavoriteSongBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FavoriteSongBackendApplication.class, args);
+
+		// 환경변수 값 확인
+		System.out.println("KAKAO_CLIENT_ID: " + System.getenv("KAKAO_CLIENT_ID"));
+		System.out.println("KAKAO_CLIENT_SECRET: " + System.getenv("KAKAO_CLIENT_SECRET"));
+		System.out.println("KAKAO_REDIRECT_URI: " + System.getenv("KAKAO_REDIRECT_URI"));
+
+		System.out.println("NAVER_CLIENT_ID: " + System.getenv("NAVER_CLIENT_ID"));
+		System.out.println("NAVER_CLIENT_SECRET: " + System.getenv("NAVER_CLIENT_SECRET"));
+		System.out.println("NAVER_REDIRECT_URI: " + System.getenv("NAVER_REDIRECT_URI"));
+
+		System.out.println("GOOGLE_CLIENT_ID: " + System.getenv("GOOGLE_CLIENT_ID"));
+		System.out.println("GOOGLE_CLIENT_SECRET: " + System.getenv("GOOGLE_CLIENT_SECRET"));
+		System.out.println("GOOGLE_REDIRECT_URI: " + System.getenv("GOOGLE_REDIRECT_URI"));
+
 	}
 
 }
